7946b866ccf2573c11f4abc550c3603d81246c60
Mariam <117775223+Jolinejo@users.noreply.github.com>
2024-04-13 13:06:16 -0700

    redis class

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.log(error.message);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const data = await getAsync(key);
+    return data;
+  }
+
+  async set(key, val, dur) {
+    const setAsync = promisify(this.client.setex).bind(this.client);
+    await setAsync(key, dur, val);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
