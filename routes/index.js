e77038177df8ac7b41b407b0ba1cd754d3b89c34
Mariam <117775223+Jolinejo@users.noreply.github.com>
2024-04-14 12:36:48 -0700

    task 3

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const { getStatus, getStats } = require('../controllers/AppController');
+const { postNew } = require('../controllers/UsersController');
 
 router.get('/status', getStatus);
 
 router.get('/stats', getStats);
+
+router.post('/users', postNew);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const router = require('./routes/index');
 
+app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
